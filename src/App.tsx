724f4d1649b6430d1b9a0c62be6bb3960ba7d1ba
el-shago <92878520+el-shago@users.noreply.github.com>
2023-09-20 23:47:36 -0700

    miau

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import Task from './components/Task.tsx';
+import Task from "./components/Task.tsx";
+import { AiOutlinePlus } from "react-icons/ai";
 
 export default function App() {
   return (
-    <div className="bg-custom font-Lexend h-full w-full">
-      <div className='flex flex-row justify-between flex-wrap items-center p-5 font-bold'>
-      <h1 className='text-white text-5xl'>DoneNow</h1>
-      <h2 className='text-white text-2xl'>Welcome back Shago!</h2>
+    <div className="bg-custom font-Lexend h-screen w-screem">
+      <div className="flex flex-row justify-between flex-wrap items-center p-10 font-bold">
+        <div className="space-y-10">
+          <h1 className="text-white text-5xl">DoneNow</h1>
+          <button className="flex flex-row bg-white rounded-full p-4 items-center">
+            <AiOutlinePlus classname="flex flex-row items-center"/> 
+            Create task
+          </button>
+        </div>
+        <h2 className="text-white text-2xl">Welcome back Shago!</h2>
       </div>
-      <button className='bg-white rounded-full p-2'>Create task</button>
-      <div className='rounded-lg bg-[#454545] m-3'>
-      <Task />
+
+      <div className="rounded-2xl bg-[#454545] m-8 space-y-6 p-5 h-4/6">
+        <Task />
+        <Task />
       </div>
     </div>
-    
-  )
+  );
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,6 +1,14 @@
 export function Task(){
     return(
-        <div>sexo</div>
+        <div className="m-8 rounded-lg bg-[#979797] font-Lexend">
+            <div className="flex justify-between text-white">
+            <div className="flex flex-col m-5">
+                <div className="flex flex-row font-bold">Hacer tarea</div>
+                <div className="flex flex-row"> hacerla ahora pq ...</div>
+            </div>
+            <div className="flex flex-col justify-center m-3 font-bold">Due date: fecha</div>
+            </div>
+        </div>
     )
 }
 
