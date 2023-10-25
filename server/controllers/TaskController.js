fd0d3cceb6bcaea635732edfacebc879b36ec965
el-shago <92878520+el-shago@users.noreply.github.com>
2023-10-24 22:00:46 -0700

    controller no funciona

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -1,16 +1,10 @@
 const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient('VITE_SUPABASE_URL', 'VITE_SUPABASE_ANON_KEY');
+const { supabase } = require('@supabase/supabase-js');
 
 const allTasks = async (req, res) => {
   try {
-    const { data, error } = await supabase.from('tasks').select('*');
-
-    if (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Error al obtener las tareas' });
-    }
-
-    res.json(data);
+    const { data, error } = await supabase.from('tasks').select('*')
+    .then(() => if (error) throw error) else;
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error en el servidor' });
