981abc0085c1ccecf490ca1254e14d4f4beeb18d
el-shago <92878520+el-shago@users.noreply.github.com>
2023-10-28 16:36:22 -0700

    sexoinnit

--- server/server.js
+++ server/server.js
@@ -1,24 +1,52 @@
-const express = require ('express');
-const routes = require('./routes/TasksRoute'); // import the routes
-require('env').config({ path: '../.env' }); // Asegúrate de ajustar la ruta adecuada al archivo .env de Vite
-const apiKey = process.env.API_KEY; // Reemplaza API_KEY con el nombre de la variable que contiene tu clave de la base de datos
+import express from "express";
+import dotenv from "dotenv"; // Asegúrate de ajustar la ruta adecuada al archivo .env de Vite
+import { supabase } from "./supabaseClient.js";
 
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/', routes); //to use the routes
+//Get tasks
+const getTasks = app.get("/", async (req, res) => {
+  try {
+    const { data: tasks, error } = await supabase.from("tasks").select("*");
+
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Error at returning task list" });
+    }
+
+    res.status(200).json({ tasks });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server Error" });
+  }
+});
+
+const createTask = app.post("/create/task", async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from("tasks")
+      .insert([{ title: "task_title", description: "task_desctiption", due_date : "task_due_date" }])
+      .select();
+
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Error at creating the task" });
+    }
+
+    res.status(200).json({ tasks });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-    console.log('Your app is listening on port ' + listener.address().port)
-
-    //console.log(import.meta.env.VITE_SUPABASE_URL);
-    //console.log(process.env.VITE_SUPABASE_ANON_KEY);
-    console.log(apiKey);
-})
-
-
+  console.log("Your app is listening on port " + listener.address().port);
+});
 
 // Ahora puedes usar apiKey en tu lógica del servidor
 // Ejemplo:
