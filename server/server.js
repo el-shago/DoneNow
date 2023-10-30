bafb2e0946a0e27d054a24a9cd27840d41e2c2b1
el-shago <92878520+el-shago@users.noreply.github.com>
2023-10-29 18:04:43 -0700

    Delete task endpoint

--- server/server.js
+++ server/server.js
@@ -47,6 +47,20 @@ const createTask = app.post("/create/task", async (req, res) => {
   }
 });
 
+const deleteTask = app.delete("/delete/task/:id", async (req, res) => {
+  try{
+    const { tasks, error } = await supabase .from("tasks").delete().match({ task_id: req.params.id });
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Error at deleting the task" });
+    }
+    res.status(200).json({ tasks });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
